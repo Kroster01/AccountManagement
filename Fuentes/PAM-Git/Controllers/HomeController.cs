6d26076675cba1b0c59b524b4dc759cca07fefc7
Jose Hernandez Q <jhernanq@everis.com>
2017-04-15 21:13:15 -0300

    Se implementa la carga por default  de la vista Index

---
+++ Fuentes/PAM-Git/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace PAM.Presentation.MVC4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController() 
+        {
+            
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
