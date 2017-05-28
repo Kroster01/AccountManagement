3090503601c82213cd515087c7896bc33a9f0f16
Jose Hernández Q <jhernanq@everis.com>
2017-05-28 10:05:57 -0400

    first part of: implement Login

--- Fuentes/PAM.Presentation.MVC4/App_Start/RouteConfig.cs
+++ Fuentes/PAM.Presentation.MVC4/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PAM.Presentation.MVC4
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional });
+                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
---
+++ Fuentes/PAM.Presentation.MVC4/Controllers/IndexController.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace PAM.Presentation.MVC4.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IndexController() 
+        {
+        }
+
+        public ActionResult Index()
+        {
+            bool isDevelopmentMode = false;
+            var isDevelopmentModeString = ConfigurationManager.AppSettings["IsDevelopment"];
+            isDevelopmentMode = bool.TryParse(isDevelopmentModeString, out isDevelopmentMode);
+            
+            if (isDevelopmentMode)
+            {
+                return PartialView("../Shared/BaseContent");
+                ///return PartialView("../Shared/RegisterUser");
+            }
+            else
+            {
+                return PartialView("../Shared/Login");
+                ///return PartialView("../Shared/RegisterUser");
+            }
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
